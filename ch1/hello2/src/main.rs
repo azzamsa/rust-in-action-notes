036185a53d304aff5a15ac4b096bffa6e31adde0
azzamsa <me@azzamsa.com>
2021-11-25 05:03:02 +0700

    test: initial

--- ch1/hello2/src/main.rs
+++ ch1/hello2/src/main.rs
@@ -12,6 +12,36 @@ fn greet_world() {
     }
 }
 
+/// The testable version
+fn greet_world2() -> [&'static str; 2] {
+    println!("Hello, world!");
+
+    let southern_germany = "Grüß Gott!";
+    let japan = "ハロー・ワールド";
+
+    [southern_germany, japan]
+}
+
 fn main() {
     greet_world();
+
+    let regions = greet_world2();
+    for region in regions {
+        println!("{}", &region)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn greet2() {
+        let southern_germany = "Grüß Gott!";
+        let japan = "ハロー・ワールド";
+
+        let regions = [southern_germany, japan];
+
+        assert_eq!(greet_world2(), regions);
+    }
 }
